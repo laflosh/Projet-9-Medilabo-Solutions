8e6b29827e48db5b6f5e241bf700e3ae5c614c9e
laflosh <l_florent_l@hotmail.fr>
2025-03-17 16:45:00 +0100

    Initial structure pour micro service patient

---
+++ mservice-patient/mservice-patient/src/main/java/com/mservice_patient/mservice_patient/MservicePatientApplication.java
@@ -0,0 +1,13 @@
+package com.mservice_patient.mservice_patient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MservicePatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MservicePatientApplication.class, args);
+	}
+
+}
---
+++ mservice-patient/mservice-patient/src/test/java/com/mservice_patient/mservice_patient/MservicePatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mservice_patient.mservice_patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MservicePatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
