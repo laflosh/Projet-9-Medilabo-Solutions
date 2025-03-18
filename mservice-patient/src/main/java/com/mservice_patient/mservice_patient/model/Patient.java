3ef266c041664af2994adf926606ae8c5ea7a5fc
laflosh <l_florent_l@hotmail.fr>
2025-03-18 12:59:38 +0100

    mservice-patient: add entity for patient

---
+++ mservice-patient/src/main/java/com/mservice_patient/mservice_patient/model/Patient.java
@@ -0,0 +1,35 @@
+package com.mservice_patient.mservice_patient.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Patient {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "birth_date")
+	private String birthDate;
+	
+	@Column(name = "gender")
+	private String gender;
+	
+	@Column(name = "address")
+	private String address;
+	
+	@Column(name = "phone_number")
+	private String phoneNumber;
+	
+}
