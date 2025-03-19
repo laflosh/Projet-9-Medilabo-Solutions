6a3f61ba85c5146991f79499794e62e8ac93ee34
laflosh <l_florent_l@hotmail.fr>
2025-03-19 18:13:18 +0100

    mservice-patient : config for connecting to eureka server

--- mservice-patient/src/main/java/com/medilabo/mservice_patient/MservicePatientApplication.java
+++ mservice-patient/src/main/java/com/medilabo/mservice_patient/MservicePatientApplication.java
@@ -4,8 +4,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MservicePatientApplication {
 
 	private final static Logger log = LogManager.getLogger("MservicePatientApplication");
