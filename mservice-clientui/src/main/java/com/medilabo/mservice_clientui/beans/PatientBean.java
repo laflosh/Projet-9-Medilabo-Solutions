d01150c73065298bea3f8cfebcdd3fb76eaa6608
laflosh <l_florent_l@hotmail.fr>
2025-03-20 15:46:46 +0100

    mservice-clientui :bean for patient entity

---
+++ mservice-clientui/src/main/java/com/medilabo/mservice_clientui/beans/PatientBean.java
@@ -0,0 +1,90 @@
+package com.medilabo.mservice_clientui.beans;
+
+public class PatientBean {
+
+	private int id;
+	
+	private String name;
+	
+	private String firstName;
+	
+	private String birthDate;
+	
+	private String gender;
+	
+	private String address;
+	
+	private String phoneNumber;
+	
+	@Override
+	public String toString() {
+
+		return "Patient {" +
+						" id : " + id +
+						", name : " + name +
+						", firstName : " + firstName +
+						", birthDate : " + birthDate +
+						", gender : " + gender +
+						", address : " + address +
+						", phoneNumber : " + phoneNumber +
+						"}";
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+}
