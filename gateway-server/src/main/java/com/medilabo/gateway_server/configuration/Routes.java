78679feef345904c8423b1f670bfbe6fab458b62
laflosh <l_florent_l@hotmail.fr>
2025-03-20 13:30:02 +0100

    gateway : configuration bean for route

---
+++ gateway-server/src/main/java/com/medilabo/gateway_server/configuration/Routes.java
@@ -0,0 +1,23 @@
+package com.medilabo.gateway_server.configuration;
+
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
+public class Routes {
+
+	@Bean
+	public RouterFunction<ServerResponse> patientServiceRoute(){
+		
+		return GatewayRouterFunctions.route("mservice-patient")
+				.route(RequestPredicates.path("/api/patients/**"), HandlerFunctions.http("http://localhost:9002"))
+				.build();
+		
+	}
+	
+}
