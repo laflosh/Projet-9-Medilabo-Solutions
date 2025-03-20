8f06d7cf0b4a459a185dbdb49174ae3e8ae4e63b
laflosh <l_florent_l@hotmail.fr>
2025-03-20 13:51:52 +0100

    initialize clientui project

---
+++ mservice-clientui/src/main/java/com/medilabo/mservice_clientui/MserviceClientuiApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.mservice_clientui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MserviceClientuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MserviceClientuiApplication.class, args);
+	}
+
+}
---
+++ mservice-clientui/src/test/java/com/medilabo/mservice_clientui/MserviceClientuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.mservice_clientui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MserviceClientuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
