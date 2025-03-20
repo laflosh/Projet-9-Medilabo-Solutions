da4499b81eea12d885ac82823fdca78ffc3cc9bd
laflosh <l_florent_l@hotmail.fr>
2025-03-20 12:57:19 +0100

    gateway-server : config gateway

--- gateway-server/src/main/java/com/medilabo/gateway_server/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/medilabo/gateway_server/GatewayServerApplication.java
@@ -1,12 +1,19 @@
 package com.medilabo.gateway_server;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayServerApplication {
 
+	private final static Logger log = LogManager.getLogger("GatewayServerApplication");
+	
 	public static void main(String[] args) {
+		log.info("Initialize gateway server");
 		SpringApplication.run(GatewayServerApplication.class, args);
 	}
 
