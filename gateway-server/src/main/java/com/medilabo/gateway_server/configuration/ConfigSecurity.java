1895f7996e2f22fbe76700c8fcfb744f0a4d4bc4
laflosh <l_florent_l@hotmail.fr>
2025-03-23 18:43:38 +0100

    gateway server : config security based on httpbasic form and in memory user for authentication

---
+++ gateway-server/src/main/java/com/medilabo/gateway_server/configuration/ConfigSecurity.java
@@ -0,0 +1,75 @@
+package com.medilabo.gateway_server.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * 
+ */
+@Configuration
+@EnableWebSecurity
+public class ConfigSecurity {
+
+	/**
+	 * @param http
+	 * @return
+	 * @throws Exception
+	 */
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		http.authorizeHttpRequests(auth -> auth
+				.requestMatchers("/").permitAll()
+				.requestMatchers("/patients").authenticated()
+				.requestMatchers("/patients/**").authenticated()
+				.requestMatchers("/api/patients/**").authenticated()
+				)
+			.httpBasic(httpBasic -> httpBasic
+					.realmName("MediLabo")
+					)
+			.logout(logout -> logout
+					.logoutUrl("/logout")
+        			.logoutSuccessUrl("/")
+        			.invalidateHttpSession(true)
+        			.deleteCookies("JSESSIONID")
+				);
+			
+		return http.build();
+		
+	}
+	
+	/**
+	 * @return
+	 */
+	@Bean
+	public UserDetailsService userDetailsInMemory() {
+		
+		UserDetails user = User.builder()
+				.username("user")
+				.password(passwordEncoder().encode("password"))
+				.roles("USER")
+				.build();
+		
+		return new InMemoryUserDetailsManager(user);
+		
+	}
+	
+	/**
+	 * @return
+	 */
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		
+		return new BCryptPasswordEncoder();
+		
+	}
+	
+}
