f1f85ad3489f2c8d6936943ad65d9d4d37191072
laflosh <l_florent_l@hotmail.fr>
2025-04-04 14:00:17 +0200

    Initialize mservice-note

---
+++ mservice-note/src/test/java/com/medilabo/mservice_note/MserviceNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.mservice_note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MserviceNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
