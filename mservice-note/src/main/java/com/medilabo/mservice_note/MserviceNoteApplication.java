0805f51e687d20de5c8f8d11dbc5e9c4b094e423
laflosh <l_florent_l@hotmail.fr>
2025-04-04 14:41:48 +0200

    mservice-note: configuration .properties file, add dependances in pom file and add data for test

--- mservice-note/src/main/java/com/medilabo/mservice_note/MserviceNoteApplication.java
+++ mservice-note/src/main/java/com/medilabo/mservice_note/MserviceNoteApplication.java
@@ -1,5 +1,7 @@
 package com.medilabo.mservice_note;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,8 +10,13 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class MserviceNoteApplication {
 
+	private final static Logger log = LogManager.getLogger("MserviceNoteApplication");
+	
 	public static void main(String[] args) {
+		
+		log.info("Initialize mservice-note server");
 		SpringApplication.run(MserviceNoteApplication.class, args);
+		
 	}
 
 }
