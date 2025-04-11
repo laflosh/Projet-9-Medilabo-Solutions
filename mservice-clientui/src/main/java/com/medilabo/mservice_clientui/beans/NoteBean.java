a68dd3f632eabb3230318f00c553eec47cb50c6d
laflosh <l_florent_l@hotmail.fr>
2025-04-11 15:23:13 +0200

    mservice-clientui: bean for note entity

---
+++ mservice-clientui/src/main/java/com/medilabo/mservice_clientui/beans/NoteBean.java
@@ -0,0 +1,45 @@
+package com.medilabo.mservice_clientui.beans;
+
+public class NoteBean {
+
+	private String id;
+	
+	private int patId;
+	
+	private String patient;
+	
+	private String note;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getPatId() {
+		return patId;
+	}
+
+	public void setPatId(int patId) {
+		this.patId = patId;
+	}
+
+	public String getPatient() {
+		return patient;
+	}
+
+	public void setPatient(String patient) {
+		this.patient = patient;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+	
+}
