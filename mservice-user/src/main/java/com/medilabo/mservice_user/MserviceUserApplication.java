28e027650e7608a12fad2c7897f274d27807b3db
laflosh <l_florent_l@hotmail.fr>
2025-04-18 16:34:28 +0200

    initialize mservice-user

---
+++ mservice-user/src/main/java/com/medilabo/mservice_user/MserviceUserApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.mservice_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MserviceUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MserviceUserApplication.class, args);
+	}
+
+}
---
+++ mservice-user/src/test/java/com/medilabo/mservice_user/MserviceUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.mservice_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MserviceUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
