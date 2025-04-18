bda0244166940a58c3add3496bb4adb000571e56
laflosh <l_florent_l@hotmail.fr>
2025-04-18 17:37:30 +0200

    mservice-user: add depedencies in pom file and .properties configuration

--- mservice-user/src/main/java/com/medilabo/mservice_user/MserviceUserApplication.java
+++ mservice-user/src/main/java/com/medilabo/mservice_user/MserviceUserApplication.java
@@ -2,8 +2,10 @@ package com.medilabo.mservice_user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MserviceUserApplication {
 
 	public static void main(String[] args) {
