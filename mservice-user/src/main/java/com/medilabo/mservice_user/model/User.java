33fdb8476832030151ab3eca82e8504d88317c2e
laflosh <l_florent_l@hotmail.fr>
2025-04-18 17:46:32 +0200

    mservice-user: implement user model

---
+++ mservice-user/src/main/java/com/medilabo/mservice_user/model/User.java
@@ -0,0 +1,116 @@
+package com.medilabo.mservice_user.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "username")
+	private String username;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "mail")
+	private String mail;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "birth_date")
+	private String birthDate;
+	
+	@Column(name = "creation_date")
+	private Date creationDate;
+	
+	@Column(name = "role")
+	private String role;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+}
