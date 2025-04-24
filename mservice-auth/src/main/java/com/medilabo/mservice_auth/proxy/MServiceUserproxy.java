8b80606671ee2c80f123c545051e2cf0894b1a30
laflosh <l_florent_l@hotmail.fr>
2025-04-24 19:56:26 +0200

    mservice-auth: implement interface for http request to mservice-user

---
+++ mservice-auth/src/main/java/com/medilabo/mservice_auth/proxy/MServiceUserproxy.java
@@ -0,0 +1,21 @@
+package com.medilabo.mservice_auth.proxy;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.medilabo.mservice_auth.dto.UserDTO;
+
+/**
+ * 
+ */
+@FeignClient(name = "mservice-user", url = "localhost:8080")
+public interface MServiceUserproxy {
+
+	@GetMapping("/api/users/username/{username}")
+	UserDTO getOneUserByUsername(@PathVariable("username") String username);
+	
+	@GetMapping("/api/users/mail/{mail}")
+	UserDTO getOneUserByMail(@PathVariable("mail") String mail);
+	
+}
