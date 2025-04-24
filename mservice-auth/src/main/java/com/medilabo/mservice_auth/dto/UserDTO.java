9d9c4c4df5bf62818155b1a1e26d7cb0c8338b31
laflosh <l_florent_l@hotmail.fr>
2025-04-24 19:51:30 +0200

    mservice-auth : implementation user dto for user model from mservice-user

---
+++ mservice-auth/src/main/java/com/medilabo/mservice_auth/dto/UserDTO.java
@@ -0,0 +1,100 @@
+package com.medilabo.mservice_auth.dto;
+
+import java.util.Date;
+
+/**
+ * 
+ */
+public class UserDTO {
+
+	private int id;
+	
+	private String username;
+	
+	private String name;
+	
+	private String firstName;
+	
+	private String mail;
+	
+	private String password;
+	
+	private String birthDate;
+	
+	private Date creationDate;
+	
+	private String role;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+}
