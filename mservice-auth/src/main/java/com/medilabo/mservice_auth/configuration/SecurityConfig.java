c5fdc59e65bca2bfc3b919b7a68976b36d0c1900
laflosh <l_florent_l@hotmail.fr>
2025-04-25 21:19:42 +0200

    mservice-auth: implementation securityconfig with authentication manager

---
+++ mservice-auth/src/main/java/com/medilabo/mservice_auth/configuration/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.medilabo.mservice_auth.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.medilabo.mservice_auth.service.CustomUserDetailsService;
+
+/**
+ * 
+ */
+@Configuration
+public class SecurityConfig {
+
+	@Autowired
+	CustomUserDetailsService customUserDetailsService;
+	
+	/**
+	 * @param http
+	 * @param bCryptPasswordEncoder
+	 * @return
+	 * @throws Exception
+	 */
+	@Bean
+	public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder) throws Exception {
+		
+		AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+		authenticationManagerBuilder.userDetailsService(customUserDetailsService).passwordEncoder(bCryptPasswordEncoder);
+		
+		return authenticationManagerBuilder.build();
+		
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		
+		return new BCryptPasswordEncoder();
+		
+	}
+	
+}
