c2b35b3a621ab3196dc748707701abcc5118cdb5
laflosh <l_florent_l@hotmail.fr>
2025-04-30 01:31:28 +0200

    gateway: implement jwtfilter

--- gateway-server/src/main/java/com/medilabo/gateway_server/filters/JwtFilter.java
+++ gateway-server/src/main/java/com/medilabo/gateway_server/filters/JwtFilter.java
@@ -1,8 +1,77 @@
 package com.medilabo.gateway_server.filters;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+
+import com.medilabo.gateway_server.service.CustomUserDetailsService;
+import com.medilabo.gateway_server.utils.JwtUtils;
+
+import jakarta.ws.rs.core.HttpHeaders;
+import reactor.core.publisher.Mono;
+
 /**
  * 
  */
-public class JwtFilter {
+@Component
+public class JwtFilter implements WebFilter {
+
+	@Autowired
+	CustomUserDetailsService userDetailsService;
+	
+	@Autowired
+	JwtUtils jwtUtils;
+	
+	/**
+	 *
+	 */
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+		
+		String authHeaders = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+		
+		String username;
+		String jwt = authHeaders.substring(7);
+		
+		if(authHeaders != null && authHeaders.startsWith("Bearer ")) {
+
+			username = jwtUtils.extractUsername(jwt);
+			
+			if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+				
+	            return userDetailsService.findByUsername(username)
+	                    .flatMap(userDetails -> {
+	                    	
+	                        if (jwtUtils.validateToken(jwt, userDetails)) {
+	                        	
+	                            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+	                                    userDetails, null, userDetails.getAuthorities());
+	                            
+	                            SecurityContextHolder.getContext().setAuthentication(auth);
+	                            
+	                        }
+	                        
+	                        return chain.filter(exchange);
+	                        
+	                    })
+	                    .onErrorResume(e -> {
+	                    	
+	                        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+	                        return exchange.getResponse().setComplete();
+	                        
+	                    });
+			
+			}
+			
+		}
+		
+		return chain.filter(exchange);
+		
+	}
 
 }
