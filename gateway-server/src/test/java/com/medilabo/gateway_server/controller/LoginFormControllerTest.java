ed110b0086f8b19bdf04845f946814688aa69869
laflosh <l_florent_l@hotmail.fr>
2025-05-13 23:01:19 +0200

    gateway: test for loginformcontroller (fix issue)

---
+++ gateway-server/src/test/java/com/medilabo/gateway_server/controller/LoginFormControllerTest.java
@@ -0,0 +1,62 @@
+package com.medilabo.gateway_server.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.medilabo.gateway_server.dtos.AuthenticationRequest;
+import com.medilabo.gateway_server.dtos.AuthenticationResponse;
+import com.medilabo.gateway_server.service.AuthenticationService;
+
+import reactor.core.publisher.Mono;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+class LoginFormControllerTest {
+
+	@Autowired
+	WebTestClient webTestClient;
+	
+    @MockBean
+    AuthenticationService authenticationService;
+    
+    private final String VALID_USERNAME = "testuser";
+    private final String VALID_PASSWORD = "testpass";
+    private final String JWT_TOKEN = "fake-jwt-token";
+	
+    @BeforeEach
+    void setUp() {
+    	
+    	AuthenticationResponse authResponse = new AuthenticationResponse();
+    	authResponse.setToken(JWT_TOKEN);
+    	
+    	Mockito.when(authenticationService.authentication(Mockito.any(AuthenticationRequest.class))).thenReturn(Mono.just(authResponse));
+    	
+    }
+    
+	@Test
+	public void loginFromHtmlFormShouldSetUpCookieAndRedirect() {
+	
+		//Testing request
+		webTestClient.post()
+			.uri("/auth/login")
+			.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+			.bodyValue("username=" + VALID_USERNAME + "&password=" + VALID_PASSWORD)
+			.exchange()
+			.expectStatus().isEqualTo(HttpStatus.FOUND)
+			.expectHeader().valueEquals("Location", "/ui/patients")
+		    .expectHeader().values("Set-Cookie", Matchers.not(Matchers.empty()))
+		    .expectHeader().valueEquals("Set-Cookie", "HttpOnly")
+		    .expectHeader().valueEquals("Set-Cookie", "Path=/");
+		
+	}
+
+}
